d888069ee786b7a9efe4b57037ac30b1dcdbfc2f
r.organ <r.organ@dan-it.com.ua>
2026-01-06 19:47:34 +0200

    convex setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,10 @@
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Mono } from "next/font/google";
 
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
-
-import { ThemeProvider } from "@/components/theme-provider";
-import { dark } from "@clerk/themes";
+import { Providers } from "@/components/providers";
 
 import "./globals.css";
+import { Authenticated } from "convex/react";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -37,36 +28,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-      appearance={{
-        theme: dark,
-      }}
-    >
-      <html lang="en" suppressHydrationWarning>
-        <body className={`${inter.variable} ${plexMono.variable} antialiased`}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <header>
-              <SignedOut>
-                <SignInButton />
-                <SignUpButton>
-                  <button className="bg-rose-500 text-white p-2 rounded">
-                    Sign up
-                  </button>
-                </SignUpButton>
-              </SignedOut>
-              <SignedIn>
-                <UserButton />
-              </SignedIn>
-            </header>
-            {children}
-          </ThemeProvider>
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.variable} ${plexMono.variable} antialiased`}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 const Home = () => {
+  const projects = useQuery(api.projects.get);
+  const createProject = useMutation(api.projects.create);
+
   return (
     <div>
       <Button>Click me</Button>
