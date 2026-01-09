63088bb831470cbc5590f06781f15bf6f11fd06e
r.organ <r.organ@dan-it.com.ua>
2026-01-09 11:29:52 +0200

    upd sentry setup

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -16,4 +16,8 @@ Sentry.init({
   // Enable sending user PII (Personally Identifiable Information)
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
+  integrations: [
+    Sentry.vercelAIIntegration,
+    Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
+  ],
 });
